0376f285052d94bf15e5d4c6425411ce976a293c
Kelston Dosh <kdosh@asu.edu>
2024-05-21 22:01:19 -0700

    refactor Brands page

---
+++ app/ui/components/TrustedBrands.tsx
@@ -0,0 +1,188 @@
+import Image from "next/image";
+
+export default function TrustedBrands() {
+  return (
+    <div className="grid grid-cols-5 gap-3 bg-white m-5 p-5 rounded-2xl">
+      <KlipchLogo></KlipchLogo>
+      <AraknisLogo></AraknisLogo>
+      <DenonLogo></DenonLogo>
+      <GeovisionLogo></GeovisionLogo>
+      <JBLLogo></JBLLogo>
+      <LaborSavingDevicesLogo></LaborSavingDevicesLogo>
+      <LutronLogo></LutronLogo>
+      <ProControlLogo></ProControlLogo>
+      <NECLogo></NECLogo>
+      <OvrCLogo></OvrCLogo>
+      <RTILogo></RTILogo>
+      <SanusLogo></SanusLogo>
+      <SonosLogo></SonosLogo>
+      <WattBoxLogo></WattBoxLogo>
+      <WilsonElectronicsLogo></WilsonElectronicsLogo>
+    </div>
+  );
+}
+
+function KlipchLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/Klipsch - Logo - black.png"
+      alt="Klipch Logo"
+      width={2728}
+      height={617}
+    ></Image>
+  );
+}
+
+function AraknisLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/Araknis-overview-header.png"
+      alt="Araknis Logo"
+      width={1025}
+      height={400}
+    ></Image>
+  );
+}
+
+function DenonLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/Denon2.png"
+      alt="Denon Logo"
+      width={414}
+      height={122}
+    ></Image>
+  );
+}
+
+function GeovisionLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/Geovision.png"
+      alt="Geovision Logo"
+      width={452}
+      height={110}
+    ></Image>
+  );
+}
+
+function JBLLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/JBL pro.png"
+      alt="JBL Logo"
+      width={275}
+      height={183}
+    ></Image>
+  );
+}
+
+function LaborSavingDevicesLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/Labor saving devices.png"
+      alt="Labor Saving Devices Logo"
+      width={318}
+      height={159}
+    ></Image>
+  );
+}
+
+function LutronLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/Lutron.png"
+      alt="Lutron Logo"
+      width={432}
+      height={116}
+    ></Image>
+  );
+}
+
+function ProControlLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/procontrol.png"
+      alt="Pro Control Logo"
+      width={2400}
+      height={900}
+    ></Image>
+  );
+}
+
+function NECLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/NEC2.jpg"
+      alt="NEC Logo"
+      width={304}
+      height={166}
+    ></Image>
+  );
+}
+
+function OvrCLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/OvrC-Logo.png"
+      alt="OvrC Logo"
+      width={325}
+      height={300}
+    ></Image>
+  );
+}
+
+function RTILogo() {
+  return (
+    <Image
+      src="/TrustedBrands/rtilogo_slogan.jpg"
+      alt="RTI Logo"
+      width={3900}
+      height={1620}
+    ></Image>
+  );
+}
+
+function SanusLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/sanus.png"
+      alt="Sanus Logo"
+      width={281}
+      height={179}
+    ></Image>
+  );
+}
+
+function SonosLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/Sonos.jpg"
+      alt="Sonos Logo"
+      width={204}
+      height={204}
+    ></Image>
+  );
+}
+
+function WattBoxLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/wattbox.png"
+      alt="Watt Box Logo"
+      width={318}
+      height={159}
+    ></Image>
+  );
+}
+
+function WilsonElectronicsLogo() {
+  return (
+    <Image
+      src="/TrustedBrands/wilson cert installer trans.png"
+      alt="Wilson Electronics Logo"
+      width={221}
+      height={228}
+    ></Image>
+  );
+}
