17b34d759147e2d796f44f0c2cbb53bccf18f5ec
Kelston Dosh <kdosh@asu.edu>
2024-05-21 20:44:50 -0700

    Updated Brand Logos

--- app/preferred-equipment-brands/page.tsx
+++ app/preferred-equipment-brands/page.tsx
@@ -1,17 +1,100 @@
-import {
-  AudioSystemBrands,
-  TVBrands,
-  TVMountBrands,
-} from "@/app/ui/components/BrandSections";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div>
       <div className="h-24"></div>
-      <div className="flex flex-col divide-y-2 divide-white m-5 gap-10">
-        <TVBrands></TVBrands>
-        <TVMountBrands></TVMountBrands>
-        <AudioSystemBrands></AudioSystemBrands>
+      <div className="grid grid-cols-5 gap-3 bg-white m-5 p-5 rounded-2xl">
+        <Image
+          src="/TrustedBrands/Klipsch - Logo - black.png"
+          alt="Klipch Logo"
+          width={2728}
+          height={617}
+        ></Image>
+        <Image
+          src="/TrustedBrands/Araknis-overview-header.png"
+          alt="Araknis Logo"
+          width={1025}
+          height={400}
+        ></Image>
+        <Image
+          src="/TrustedBrands/Denon2.png"
+          alt="Denon Logo"
+          width={414}
+          height={122}
+        ></Image>
+        <Image
+          src="/TrustedBrands/Geovision.png"
+          alt="Geovision Logo"
+          width={452}
+          height={110}
+        ></Image>
+        <Image
+          src="/TrustedBrands/JBL pro.png"
+          alt="JBL Logo"
+          width={275}
+          height={183}
+        ></Image>
+        <Image
+          src="/TrustedBrands/Labor saving devices.png"
+          alt="Labor Saving Devices Logo"
+          width={318}
+          height={159}
+        ></Image>
+        <Image
+          src="/TrustedBrands/Lutron.png"
+          alt="Lutron Logo"
+          width={432}
+          height={116}
+        ></Image>
+        <Image
+          src="/TrustedBrands/procontrol.png"
+          alt="Pro Control Logo"
+          width={2400}
+          height={900}
+        ></Image>
+        <Image
+          src="/TrustedBrands/NEC2.jpg"
+          alt="NEC Logo"
+          width={304}
+          height={166}
+        ></Image>
+        <Image
+          src="/TrustedBrands/OvrC-Logo.png"
+          alt="OvrC Logo"
+          width={325}
+          height={300}
+        ></Image>
+        <Image
+          src="/TrustedBrands/rtilogo_slogan.jpg"
+          alt="RTI Logo"
+          width={3900}
+          height={1620}
+        ></Image>
+        <Image
+          src="/TrustedBrands/sanus.png"
+          alt="Sanus Logo"
+          width={281}
+          height={179}
+        ></Image>
+        <Image
+          src="/TrustedBrands/Sonos.jpg"
+          alt="Sonos Logo"
+          width={204}
+          height={204}
+        ></Image>
+        <Image
+          src="/TrustedBrands/wattbox.png"
+          alt="Watt Box Logo"
+          width={318}
+          height={159}
+        ></Image>
+        <Image
+          src="/TrustedBrands/wilson cert installer trans.png"
+          alt="Wilson Electronics Logo"
+          width={221}
+          height={228}
+        ></Image>
       </div>
     </div>
   );
