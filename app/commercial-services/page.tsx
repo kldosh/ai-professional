b22750dcf5f4d67a0393b7fae8f2f3527310c5e5
Kelston Dosh <kdosh@asu.edu>
2024-05-24 18:01:42 -0700

    edited hero

--- app/commercial-services/page.tsx
+++ app/commercial-services/page.tsx
@@ -1,35 +1,11 @@
-import Hero from "@/app/ui/components/Hero/Hero";
-import Image from "next/image";
 import { CommercialServiceCards } from "@/app/ui/components/CardGroups";
+import { CommercialServicesHero } from "@/app/ui/components/Hero/CommercialServicesHero";
 
 export default function Page() {
   return (
     <div>
       <div className="h-24"></div>
-      <Hero
-        title={
-          <h1 className="text-7xl font-bold leading-tight mb-4 drop-shadow-outline">
-            Commercial Services
-          </h1>
-        }
-        text={
-          <p className="text-2xl text-white mb-8 w-2/3 drop-shadow-outline">
-            AI Professional provides an array of services and products. From
-            project design, consultation, product recommendation and acquisition
-            to project implementation, end user t raining, and post project
-            completion support. System service/maintenance contracts available.
-          </p>
-        }
-        image={
-          <Image
-            src="/Commercial-TV-Mounting-2.JPG"
-            alt=""
-            width={3264}
-            height={2448}
-            className="object-cover object-center w-full h-full"
-          ></Image>
-        }
-      ></Hero>
+      <CommercialServicesHero></CommercialServicesHero>
       <CommercialServiceCards></CommercialServiceCards>
     </div>
   );
---
+++ app/ui/components/Hero/CommercialServicesHero.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+import Hero from "@/app/ui/components/Hero/Hero";
+
+export function CommercialServicesHero() {
+  return (
+    <Hero
+      title={
+        <h1 className="text-7xl font-bold leading-tight mb-4 drop-shadow-outline">
+          Commercial Services
+        </h1>
+      }
+      text={
+        <p className="text-2xl text-white mb-8 w-2/3 drop-shadow-outline">
+          AI Professional provides an array of services and products. From
+          project design, consultation, product recommendation and acquisition
+          to project implementation, end user t raining, and post project
+          completion support. System service/maintenance contracts available.
+        </p>
+      }
+      image={
+        <Image
+          src="/Commercial-TV-Mounting-2.JPG"
+          alt=""
+          width={3264}
+          height={2448}
+          className="object-cover object-center w-full h-full"
+        ></Image>
+      }
+    ></Hero>
+  );
+}
