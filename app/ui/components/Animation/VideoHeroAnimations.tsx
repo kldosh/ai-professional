7a51804a9502aa7211537d4f678e7c7f59a4b748
Kelston Dosh <kdosh@asu.edu>
2024-05-28 21:16:06 -0700

    changed some amber backgrounds to white

--- app/ui/components/Animation/VideoHeroAnimations.tsx
+++ app/ui/components/Animation/VideoHeroAnimations.tsx
@@ -111,7 +111,7 @@ export function ContactUsLink() {
         href="/contact-us"
         className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200"
       >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-amber-50 rounded-md group-hover:bg-opacity-0">
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
           Book a Consultation Today!
         </span>
       </Link>
@@ -146,7 +146,7 @@ export function ServicesLink() {
         href="/commercial-services"
         className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200"
       >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-amber-50 rounded-md group-hover:bg-opacity-0">
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
           Commercial Services
         </span>
       </Link>
@@ -154,7 +154,7 @@ export function ServicesLink() {
         href="/residential-services"
         className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200"
       >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-amber-50 rounded-md group-hover:bg-opacity-0">
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
           Residential Services
         </span>
       </Link>
--- app/ui/components/VideoHero.tsx
+++ app/ui/components/VideoHero.tsx
@@ -15,7 +15,7 @@ export default function VideoHero({ title, text, link, src }: Props) {
     <div className="h-[200vh] relative">
       <div className="sticky top-1/2 transform -translate-y-1/3">
         <motion.div
-          className="bg-amber-50 h-auto mx-10 rounded-2xl shadow-[0px_0px_20px_10px] shadow-ai-blue"
+          className="bg-white h-auto mx-10 rounded-2xl shadow-[0px_0px_20px_10px] shadow-ai-blue"
           initial={{ opacity: 0, scale: 0.9, y: -20 }}
           whileInView={{ opacity: 1, scale: 1, y: -20 }}
           transition={{ duration: 1 }}
